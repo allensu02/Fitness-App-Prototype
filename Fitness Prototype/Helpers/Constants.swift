dd08de723a56eb8d311727b59ada11d7a5574f85
Allen Su <allenysu@gmail.com>
2020-03-30 22:39:16 +0800

    implement initial signup and login to firebase.

---
+++ Fitness Prototype/Helpers/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Fitness Prototype
+//
+//  Created by Allen Su on 2020/3/30.
+//  Copyright © 2020 Allen Su. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct Storyboard {
+        static let homeViewController = "HomeVC"
+        static let birthdayViewController = "BirthVC"
+    }
+}
---
+++ Fitness Prototype/Helpers/Utilities.swift
@@ -0,0 +1,61 @@
+//
+//  Utilities.swift
+//  customauth
+//
+//  Created by Christopher Ching on 2019-05-09.
+//  Copyright © 2019 Christopher Ching. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Utilities {
+    
+    static var color = UIColor.init(red: 51/255, green: 51/255, blue: 2551/255, alpha: 1)
+    static func styleTextField(_ textfield:UITextField) {
+        
+        // Create the bottom line
+        let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
+        
+        bottomLine.backgroundColor = color.cgColor
+        
+        // Remove border on text field
+        textfield.borderStyle = .none
+        
+        // Add the line to the text field
+        textfield.layer.addSublayer(bottomLine)
+        
+    }
+    
+    static func styleFilledButton(_ button:UIButton) {
+        
+        // Filled rounded corner style
+        button.backgroundColor = color
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.white
+    }
+    
+    static func styleHollowButton(_ button:UIButton) {
+        
+        // Hollow rounded corner style
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.black
+    }
+    
+    static func isPasswordValid(_ password : String) -> Bool {
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+    static func isEmailValid (_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+}
